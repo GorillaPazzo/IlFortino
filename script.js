e49682e3d428e4f16d290d71386dd349c92f2d88
GorillaPazzo <162300207+GorillaPazzo@users.noreply.github.com>
2024-03-13 01:40:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const inputFieldEL= document.getElementById("inputBox");
+const addButtonEL= document.getElementById("logButton");
+addButtonEL.addEventListener("click",function(){
+    let inputValue=inputFieldEL.value;
+        console.log(inputValue)
+})
