1f01cf123ee745cf87ea6bde9dd4f3c2de9de782
GorillaPazzo <162300207+GorillaPazzo@users.noreply.github.com>
2024-03-14 09:26:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
-const inputFieldEL= document.getElementById("inputBox");
-const addButtonEL= document.getElementById("logButton");
-addButtonEL.addEventListener("click",function(){
-    let inputValue=inputFieldEL.value;
-        console.log(inputValue)
-})
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+    databaseURL: "https://databasefortino-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const newsListInDB = ref(database, "notizie");
+const inputFieldEL = document.getElementById("inputBox");
+const addButtonEL = document.getElementById("logButton");
+const newsListEL = document.getElementById("news-list");
+
+addButtonEL.addEventListener("click", function() {
+    let inputValue = inputFieldEL.value;
+    push(newsListInDB, inputValue);
+    clearInputFieldEl();
+    console.log(`${inputValue} added to database`);
+});
+
+onValue(newsListInDB, function(snapshot) {
+    
+    let newsArray = Object.values(snapshot.val());
+   
+    clearNewsList()
+     
+    for(let i = 0; i < newsArray.length; i++) {
+        let currentNews = newsArray[i];
+        appendItemToNewsList(currentNews); 
+    }
+});
+
+   
+function clearNewsList(){
+    newsListEL.innerHTML = null;
+}
+
+function clearInputFieldEl(){
+    inputFieldEL.value = null;
+}
+
+function appendItemToNewsList(itemValue){
+    newsListEL.innerHTML += `<li>${itemValue}</li>`;
+}
